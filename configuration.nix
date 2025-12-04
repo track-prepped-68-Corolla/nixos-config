bbbdb8216bc6f545b3c533c39a0166aa7bb1eb98
track-prepped-68-Corolla <jsetne@pm.me>
2025-12-04 17:03:17 -0500

    added my custom nixos-hardware fork to my flake

--- configuration.nix
+++ configuration.nix
@@ -195,7 +195,7 @@
   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  gamescopeSession.enable = true;
+  gamescopeSession.enable = false;
 };
 
   # Allow unfree packages
--- flake.nix
+++ flake.nix
@@ -9,18 +9,20 @@
 
       jovian-nixos.url = "github:jovian-experiments/jovian-nixos";
 
+      nixos-hardware.url = "github:track-prepped-68-Corolla/nixos-hardware";
+
       home-manager = {
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
       };
   };
 
-outputs = { self, nixpkgs, catppuccin, home-manager, jovian-nixos, ... }@inputs:
+outputs = { self, nixpkgs, catppuccin, home-manager, jovian-nixos, nixos-hardware, ... }@inputs:
 
 {
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem{
-        specialArgs = { inherit inputs catppuccin home-manager jovian-nixos; };
+        specialArgs = { inherit inputs catppuccin home-manager jovian-nixos nixos-hardware; };
 
       modules = [
         catppuccin.nixosModules.catppuccin
@@ -28,10 +30,12 @@ outputs = { self, nixpkgs, catppuccin, home-manager, jovian-nixos, ... }@inputs:
         ./configuration.nix
         inputs.home-manager.nixosModules.default
         jovian-nixos.nixosModules.default
+        # PLACEHOLDER! DO NOT UNCOMMENT UNTIL YOU'VE POINTED THIS AT THE CORRECT SYSTEM!
+        #nixos-hardware.nixosModules.dell-xps-13-9380
         {nix.settings.experimental-features = ["nix-command" "flakes"];}
       ];
     };
 
   };
-  
+
 }
