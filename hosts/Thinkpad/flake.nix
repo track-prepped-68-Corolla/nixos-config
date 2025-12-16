c4c99875fb8db21cdda58aeed911aee8b6ae0bb9
track-prepped-68-Corolla <jsetne@pm.me>
2025-12-15 21:07:20 -0500

    added thinkpad to hosts

---
+++ hosts/Thinkpad/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "nixos config";
+
+    inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+      catppuccin.url = "github:catppuccin/nix";
+      catppuccin.inputs.nixpkgs.follows = "nixpkgs";
+
+      jovian-nixos.url = "github:jovian-experiments/jovian-nixos";
+
+      nixos-hardware.url = "github:track-prepped-68-Corolla/nixos-hardware";
+
+      home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+  };
+
+outputs = { self, nixpkgs, catppuccin, home-manager, jovian-nixos, nixos-hardware, ... }@inputs:
+
+{
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem{
+        specialArgs = { inherit inputs catppuccin home-manager jovian-nixos nixos-hardware; };
+
+      modules = [
+        catppuccin.nixosModules.catppuccin
+        home-manager.nixosModules.home-manager
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+        jovian-nixos.nixosModules.default
+        # PLACEHOLDER! DO NOT UNCOMMENT UNTIL YOU'VE POINTED THIS AT THE CORRECT SYSTEM!
+        nixos-hardware.nixosModules.asus-flow-gz301vu
+        {nix.settings.experimental-features = ["nix-command" "flakes"];}
+      ];
+    };
+
+  };
+
+}
