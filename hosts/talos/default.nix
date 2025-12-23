7c4bfe8105ca078eb7172358ec49de8402ca8e28
track-prepped-68-Corolla <jsetne@pm.me>
2025-12-23 18:42:22 -0500

    Update default.nix

--- hosts/talos/default.nix
+++ hosts/talos/default.nix
@@ -1,1 +1,58 @@
-put code here
+{ config, pkgs, lib, catppuccin, home-manager, inputs, ... }:
+
+{
+  # 1. Imports MUST be at the top level, outside of 'config'
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/
+  ];
+
+  modules = {
+    podman.enable = true;
+  };
+
+  # 2. Options (Defining the variable)
+  options.mainUser = lib.mkOption {
+    type = lib.types.str;
+    default = "joe";
+  };
+
+  # 3. Config (Setting the actual values)
+  config = {
+    # It is safe to also explicitly define this here just in case
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+    nixpkgs.config.allowUnfree = true;
+
+    networking.networkmanager.enable = true;
+
+    networking.hostName = "rog";
+
+    boot.loader.grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+    };
+
+    services = {
+      supergfxd.enable = false;
+};
+
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    # ... Include the rest of your settings (Networking, Users, etc.) here ...
+
+    users.users.${config.mainUser} = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" "docker" "incus-admin" "lp" "scanner" "printadmin" ];
+    };
+
+    home-manager = {
+      extraSpecialArgs = { inherit inputs; };
+      users.${config.mainUser} = ./../../home/users + "/${config.mainUser}";
+    };
+
+    system.stateVersion = "25.05";
+  };
+}
