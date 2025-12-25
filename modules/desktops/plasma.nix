cfc3868f6ed0e86626896860fd8dbaa0885d6a0a
track-prepped-68-Corolla <jsetne@pm.me>
2025-12-25 10:06:46 -0500

    Update plasma.nix

--- modules/desktops/plasma.nix
+++ modules/desktops/plasma.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # Enable the X11 windowing system.
+
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
 
   programs.kdeconnect.enable = true;
@@ -18,14 +17,14 @@
     kdePackages.krdc
   ];
 
-  # Configure KWallet to unlock on login
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    elisa
+  ];
+
+
   security.pam.services.kwallet = {
     name = "kwallet";
     enableKwallet = true;
   };
 
-  # Optional: Exclude some default KDE bloat
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    elisa
-  ];
 }
