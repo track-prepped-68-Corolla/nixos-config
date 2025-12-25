522e44d566abb175dc981d533142c8818db25e61
track-prepped-68-Corolla <jsetne@pm.me>
2025-12-25 10:00:53 -0500

    Update default.nix

--- modules/apps/default.nix
+++ modules/apps/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, catppuccin, home-manager, inputs, ... }:
 {
   config = {
-      environment.systemPackages = [ 
+      environment.systemPackages = with pkgs; = [ 
       pkgs.tailscale 
       pkgs.kdePackages.krdc
       ];
